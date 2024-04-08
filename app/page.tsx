9f7da1e1307ea9aba4637026d20368d045e5de82
TigranBalayan10 <tigranbalayan10@gmail.com>
2024-04-08 02:24:42 -0700

    navbar initial setup done

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Navbar from "@/components/Navigation/Navbar";
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Hello, World!</h1>
+    <main className="">
+      <Navbar />
+      
     </main>
   );
 }
---
+++ lib/types.ts
@@ -0,0 +1,9 @@
+export interface carlist {
+    title: string;
+    href: string;
+    description: string;
+    image: string;
+    price: string | number;
+    mileage: string;
+    year: string;
+  }
