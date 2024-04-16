5da81b2d4fac3632e6f2c87b0e2a81d69b6329cf
TigranBalayan10 <tigranbalayan10@gmail.com>
2024-04-16 12:20:31 -0700

    fixed bug with shadcn command component for search input

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Navbar from "@/components/Navigation/Navbar";
 import Hero from "@/components/Hero/Hero";
 import Footer from "@/components/Footer/Footer";
+// import SearchBar from "@/components/SearchBar/SearchBar";
+import ComboboxDemo from "@/components/SearchBar/ComboboxDemo";
 
 
 export default function Home() {
@@ -8,6 +10,9 @@ export default function Home() {
 <main className="flex flex-col min-h-screen">
     <Navbar />
     <Hero />
+    {/* <SearchBar /> */}
+    
+    <ComboboxDemo />
     <Footer />
 </main>
   );
