d18111ad9fa50b7b63bff2a9ac4b4ca9f5d54701
TigranBalayan10 <tigranbalayan10@gmail.com>
2024-05-13 16:47:15 -0700

    Hero section with video animation and mobile view friendly

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -5,39 +5,37 @@ import Link from "next/link";
 
 const Hero = () => {
   return (
-    <div className="mt-5 flex-grow">
-      <div className="flex bg-teal-200 h-full container mx-auto">
-        <div className="flex items-center text-center lg:text-left px-8 md:px-12 lg:w-1/2">
-          <div>
-            <h2 className="text-3xl font-semibold text-gray-800 md:text-4xl">
-              Welcome to{" "}
-              <span className="text-blue-800">M Fest MotorSport</span>
-            </h2>
-            <p className="mt-2 text-sm text-gray-500 md:text-base">
-              Welcome to M Fest MotorSport, your home for luxury BMW vehicles.
-              Explore our iconic models and start your BMW journey today!
-            </p>
-            <div className="flex justify-center lg:justify-start mt-6">
-              <Link href="/carsforsale">
-                <Button>Browse Inventory</Button>
-              </Link>
-              <Button variant="ghost" className="ml-2">
-                Learn More
-              </Button>
-            </div>
+    <div className="my-5 flex-grow mx-5">
+      <div className="container mx-auto bg-slate-600 rounded-md shadow-lg pl-0 pr-0 px-4 md:px-0">
+        <div className="md:flex">
+          <div className="relative md:w-1/2">
+            <video
+              className="w-full h-auto rounded-t-md md:rounded-l-md md:rounded-r-none"
+              autoPlay
+              muted
+              playsInline
+            >
+              <source src="/BMW logo animation.webm" type="video/webm" />
+            </video>
           </div>
-        </div>
-        <div
-          className="hidden lg:block lg:w-1/2"
-          style={{ clipPath: "polygon(10% 0, 100% 0%, 100% 100%, 0 100%)" }}
-        >
-          <div className="">
-            <Image
-              src="/bmw-landing-page.jpg"
-              width={600}
-              height={300}
-              alt="BMW landing page pic"
-            />
+          <div className="md:w-1/2 p-8 flex md:ml-auto md:flex md:items-center">
+            <div className="md:text-right text-center">
+              <h2 className="text-3xl font-semibold text-gray-400 md:text-4xl">
+                Welcome to <br></br> <span className="text-blue-400">M Fest MotorSport</span>
+              </h2>
+              <p className="mt-2 text-sm text-gray-200 md:text-base">
+                Welcome to M Fest MotorSport, your home for luxury BMW vehicles.
+                Explore our iconic models and start your BMW journey today!
+              </p>
+              <div className="flex md:justify-end justify-center mt-6">
+                <Link href="/carsforsale">
+                  <Button>Browse Inventory</Button>
+                </Link>
+                <Button variant="ghost" className="ml-2">
+                  Learn More
+                </Button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
