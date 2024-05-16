a50cb8e6c7152105c5ad514c676bd324f9610d93
TigranBalayan10 <tigranbalayan10@gmail.com>
2024-05-16 04:21:41 -0700

    styled sign in working as intended

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,12 @@
+import { Card } from "@/components/ui/card";
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn path="/sign-in" />;
+    return (
+        <div className="container mx-auto px-4 sm:px-0 p-4 flex flex-col gap-4 items-center justify-center">
+            <Card className="w-[350px] mt-5">
+                <SignIn path="/sign-in" />
+            </Card>
+        </div>
+    )
 }
